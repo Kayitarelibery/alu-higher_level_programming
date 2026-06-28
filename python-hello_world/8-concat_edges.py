9e162170025c6c34132266bfe8002e875cff3b50
Kayitarelibery <l.kayitare1@alustudent.com>
2026-06-28 08:00:22 -0700

    task 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language"
-print(str[0:6] + str[7:9] + str[10:16] + str[17:38] + str[39:52])
+word = "Holberton"
+print(word[3:8] + word[-2:] + word[:2] + word[5])
