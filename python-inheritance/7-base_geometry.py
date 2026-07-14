ea73de03a72e3913bd3379f9d705b97bb9275ed3
Kayitarelibery <you@example.com>
2026-07-14 08:41:40 +0000

    Rewrite integer_validator using isinstance with explicit bool exclusion

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -20,7 +20,7 @@ class BaseGeometry:
             TypeError: if value is not an int.
             ValueError: if value is less than or equal to 0.
         """
-        if type(value) is not int:
+        if not isinstance(value, int) or isinstance(value, bool):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
